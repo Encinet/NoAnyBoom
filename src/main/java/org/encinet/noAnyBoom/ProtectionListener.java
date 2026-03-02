4db0ceafa0d32e05d7baa6d5ab3592b16998f065
Noctiro <49872497+noctiro@users.noreply.github.com>
2026-03-02 12:28:19 +0800

    Fix

--- src/main/java/org/encinet/noAnyBoom/ProtectionListener.java
+++ src/main/java/org/encinet/noAnyBoom/ProtectionListener.java
@@ -75,6 +75,7 @@ public class ProtectionListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockExplode(BlockExplodeEvent event) {
+        if (event.getBlock().getType() == Material.AIR) return;
         event.setCancelled(true);
         WarningUtils.broadcast("exploded", "Environment", event.getBlock().getType().name(), event.getBlock().getLocation());
         ScanUtils.scanAndHandleBannedBlocks(event.getBlock().getLocation(), 5, null);
